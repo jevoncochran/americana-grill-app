ba5248941d60333a161ec246ad294dd14a05744d
Jevon Cochran <jevon@securebags.com>
2023-10-26 14:26:28 -0500

    home page Slider

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <div className="h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between">
+      <Link href="/" className="text-xl font-bold uppercase=">
+        Americana Grill
+      </Link>
+      <p>© ALL RIGHTS RESERVED</p>
+    </div>
+  );
+};
+
+export default Footer;
