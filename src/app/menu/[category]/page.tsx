5f07772709bea28df9b9196a74d0f834c915fe90
Jevon Cochran <jevon@securebags.com>
2023-10-26 17:57:53 -0500

    CategoryPage

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,5 +1,35 @@
+import { PIZZAS } from "@/data";
+import Image from "next/image";
+import Link from "next/link";
+
 const CategoryPage = () => {
-  return <div>CategoryPage</div>;
+  return (
+    <div className="flex flex-wrap text-red-500">
+      {PIZZAS.map((item) => (
+        <Link
+          key={item.id}
+          href={`/product/${item.id}`}
+          className="w-full sm:w-1/2 lg:w-1/3 h-[60vh] border-r-2 border-b-2 border-red-500 even:bg-fuchsia-50 p-4 flex flex-col justify-between group"
+        >
+          {/* IMAGE CONTAINER */}
+          {item.img && (
+            <div className="relative h-[80%]">
+              <Image src={item.img} alt="" fill className="object-contain" />
+            </div>
+          )}
+
+          {/* TEXT CONTAINER */}
+          <div className="flex items-center justify-between font-bold">
+            <h1 className="text-2xl uppercase p-2">{item.title}</h1>
+            <h2 className="group-hover:hidden text-xl">${item.price}</h2>
+            <button className="hidden group-hover:block uppercase bg-red-500 text-white p-2 rounded-md">
+              Add to cart
+            </button>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default CategoryPage;
