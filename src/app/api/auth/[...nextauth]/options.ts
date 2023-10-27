0f60a071cdb870cd9dc16689fdac424b97a78fd7
Jevon Cochran <jevon@securebags.com>
2023-10-27 13:51:44 -0500

    add prisma adapter

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,7 +1,10 @@
-import { NextAuthOptions } from "next-auth";
+import prisma from "@/utils/connect";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { NextAuthOptions, getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
@@ -9,3 +12,5 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
 };
+
+export const getAuthSession = () => getServerSession(authOptions);
