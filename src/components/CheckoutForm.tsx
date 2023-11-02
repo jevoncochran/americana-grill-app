21935b1b9a624917455766621af775787da91418
Jevon Cochran <jevon@securebags.com>
2023-11-01 22:43:56 -0500

    add https to return_url

--- src/components/CheckoutForm.tsx
+++ src/components/CheckoutForm.tsx
@@ -12,7 +12,7 @@ import AddressForm from "./AddressForm";
 const APP_URL =
   process.env.NEXT_PUBLIC_VERCEL_ENV === "development"
     ? "http://localhost:3000"
-    : process.env.NEXT_PUBLIC_VERCEL_URL;
+    : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
 
 const CheckoutForm = () => {
   const stripe = useStripe();
